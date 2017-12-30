8a578692aefd256e258206fdce8320e86b87ff24
aleszp <aleksander.szsz@gmail.com>
2017-12-30 04:06:59 +0100

    Tymczasowo zamiast wczytywać bitmapy zamalowuję pola białymi i czarnymi prostokątami. Poprawiłem wymiary i lokalizację pól, teraz są równe. Wymiary pól wynoszą 150x150 (narożne) i 150x75/75x150 (pola w kolumnach/ rzędach).

--- src/Plansza.cpp
+++ src/Plansza.cpp
@@ -23,22 +23,22 @@ Plansza::Plansza(Silnik* silnik, QWidget* parent) : QWidget(parent)
     glownyRozmieszczacz_->addWidget(pola_[0],11,11,2,2);
     for(ii=1;ii<10;ii++)//dolny rząd
     {
-        glownyRozmieszczacz_->addWidget(pola_[ii],11,11-ii,1,2);
+        glownyRozmieszczacz_->addWidget(pola_[ii],11,11-ii,2,1);
     }
     glownyRozmieszczacz_->addWidget(pola_[10],11,0,2,2);
     for(ii=11;ii<20;ii++)//lewa kolumna
     {
-        glownyRozmieszczacz_->addWidget(pola_[ii],21-ii,0,2,1);
+        glownyRozmieszczacz_->addWidget(pola_[ii],21-ii,0,1,2);
     }
     glownyRozmieszczacz_->addWidget(pola_[20],0,0,2,2);
     for(ii=21;ii<30;ii++)//górny rząd
     {
-        glownyRozmieszczacz_->addWidget(pola_[ii],0,ii-19,1,2);
+        glownyRozmieszczacz_->addWidget(pola_[ii],0,ii-19,2,1);
     }
     glownyRozmieszczacz_->addWidget(pola_[30],0,11,2,2);
     for(ii=31;ii<40;ii++)//prawa kolumna
     {
-        glownyRozmieszczacz_->addWidget(pola_[ii],ii-30,10,2,1);
+        glownyRozmieszczacz_->addWidget(pola_[ii],ii-29,11,1,2);
     }
 
     std::cout<<glownyRozmieszczacz_->rowCount()<<"x"<<glownyRozmieszczacz_->columnCount()<<std::endl;
--- src/PoleGraficzne.cpp
+++ src/PoleGraficzne.cpp
@@ -1,3 +1,6 @@
+//Nagłówki z katalogu Qt
+#include <QColor>
+
 //Standardowe nagłówki C/C++
 #include <iostream>
 
@@ -21,8 +24,27 @@ PoleGraficzne::PoleGraficzne(Plansza* rodzic, uint8_t id) : QWidget(rodzic)
     wlasciciel_->setPixmap(*wlasciciel_map_);*/
 
     ilustracja_=new QLabel(this);
-    strcpy(tmp,(sciezka+std::to_string(id)+".png").c_str());
-    ilustracja_map_=new QPixmap(tmp);
+    //strcpy(tmp,(sciezka+std::to_string(id)+".png").c_str());
+    //ilustracja_map_=new QPixmap(tmp);
+    if((id>0&&id<10)||(id>20&&id<30))       //rzędy
+    {
+        setFixedSize(75,150);
+        ilustracja_map_=new QPixmap(75,150);
+    }
+    else
+    {
+        if((id>10&&id<20)||(id>30&&id<40))       //kolumny
+        {
+            setFixedSize(150,75);
+            ilustracja_map_=new QPixmap(150,75);
+        }
+        else //narożne
+        {
+            setFixedSize(150,150);
+            ilustracja_map_=new QPixmap(150,150);
+        }
+    }
+    ilustracja_map_->fill(id%2?QColor(255,255,255):QColor(0,0,0));
     ilustracja_->setPixmap(*ilustracja_map_);
 
     /*for(uint8_t ii=0;ii<4;ii++)
@@ -32,19 +54,4 @@ PoleGraficzne::PoleGraficzne(Plansza* rodzic, uint8_t id) : QWidget(rodzic)
         gracz_map_[ii]=new QPixmap(tmp);
         gracz_[ii]->setPixmap(*gracz_map_[ii]);
     }*/
-    if((id>0&&id<10)||(id>20&&id<30))       //rzędy
-    {
-        setFixedSize(90,180);
-    }
-    else
-    {
-        if((id>10&&id<20)||(id>30&&id<40))       //kolumny
-        {
-            setFixedSize(180,90);
-        }
-        else //narożne
-        {
-            setFixedSize(180,180);
-        }
-    }
 }
